9e78c105d8520b30ce2fb67be085a418d5db705e
hjelleyman <38963036+hjelleyman@users.noreply.github.com>
2020-11-02 10:48:11 +1300

    $update_text

--- autoupdater.bat
+++ autoupdater.bat
@@ -1,5 +1,5 @@
-if "%~1"==""  set update_text="automatic updates"
-else set update_text=%1
+if "%~1"==""  SET update_text="automatic updates"
+else SET update_text=%1
 call env export > environment.yml
 git add *
 git commit -m $update_text

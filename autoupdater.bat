db19bfe783ffa7db5f6d7f654906cbf5cd6c1f96
hjelleyman <38963036+hjelleyman@users.noreply.github.com>
2020-11-02 11:05:29 +1300

    automatic updates

--- autoupdater.bat
+++ autoupdater.bat
@@ -1,5 +1,6 @@
 SET update_text=%1
-if "%~1"==""  SET update_text="automatic updates" 
+if "%~1"=="" SET /p update_text="Want to enter a commit?"
+if "%update_text%"=="" SET update_text="automatic updates" 
 call env export > environment.yml
 git add *
 git commit -m %update_text%

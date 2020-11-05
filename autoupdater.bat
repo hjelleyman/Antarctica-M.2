a992c4b70f0b52b1a04da2eeb0a2b69186c22d49
hjelleyman <38963036+hjelleyman@users.noreply.github.com>
2020-11-06 11:02:03 +1300

    removed requirements.txt

--- autoupdater.bat
+++ autoupdater.bat
@@ -1,6 +1,5 @@
 SET update_text=%1
-if "%~1"==""  SET update_text="automatic updates" 
-call env export > environment.yml
+if "%~1"==""  SET update_text="automatic updates"
 git add *
 git commit -m %update_text%
 git push

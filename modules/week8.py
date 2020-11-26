46dd0aa5d7ca667a8bc08579ce5047a84e35c687
hjelleyman <38963036+hjelleyman@users.noreply.github.com>
2020-11-27 08:45:47 +1300

    update from end of day

--- modules/week8.py
+++ modules/week8.py
@@ -7,9 +7,31 @@ import cartopy.crs as ccrs
 import numpy as np
 import itertools
 from plyer import notification
+import glob
+
+from pyproj import Proj, transform
 
 from matplotlib.colors import LinearSegmentedColormap, TwoSlopeNorm
 
+def load_landmask():
+	files = glob.glob('data/landice2/*.nc')
+	ds = xr.open_mfdataset(files)
+	ds = ds.land_mask
+	Y, X = [10*np.arange(435000,-395000,-2500),
+			10*np.arange(-395000,395000,2500)]
+	x,y = np.meshgrid(X,Y)
+	inProj = Proj(init='epsg:3031')
+	outProj = Proj(init='epsg:4326')
+	x,y = transform(inProj,outProj,x,y)
+	x = x.flatten()
+	y = y.flatten()
+	x[x<0] = x[x<0]+360 
+	x = xr.DataArray(x, dims='z')
+	y = xr.DataArray(y, dims='z')
+	newdata = xr.DataArray(dims=('y','x'),coords = [Y,X])
+	variable_data = ds.interp(lon=x, lat=y, method = 'linear', kwargs={"fill_value": 0.0})	
+	newdata.loc[:,:] = variable_data.values.reshape([len(Y),len(X)])
+	return newdata.compute()
 
 def load_ice_data():
 	"""Loads in ice data, projected to the polar stereographic grid."""
@@ -33,7 +55,7 @@ def load_temp_data():
 	return temperature
 
 
-def plot_2_timeseries(SIC, temperature):
+def plot_2_timeseries(SIC, temperature, landmask):
 
 	# Normalizing variables by area of grid cells This currently doesn't work
 	area = xr.open_dataset('data/area_files/processed_nsidc.nc').area
@@ -57,9 +79,6 @@ def plot_2_timeseries(SIC, temperature):
 		gradient_b = gradient.sel(degree=0)
 		predicted[variable] = \
 		gradient_m['polyfit_coefficients'].values * temperature.time.values.astype(float) + gradient_b['polyfit_coefficients'].values
-	predicted['sst'] = predicted['sst'] / (len(temperature.x)*len(temperature.y))
-	predicted['skt'] = predicted['skt'] / (len(temperature.x)*len(temperature.y))
-	predicted['t2m'] = predicted['t2m'] / (len(temperature.x)*len(temperature.y))
 
 	gradient = SIC.sum(dim=('x','y')).polyfit(dim='time', deg=1)
 	gradient_m = gradient.sel(degree=1)
@@ -104,7 +123,7 @@ def plot_2_timeseries(SIC, temperature):
 
 
 
-def plot_6_timeseries(SIC, LIC, temperature):
+def plot_6_timeseries(SIC, LIC, temperature, landmask):
 
 	# Normalizing variables by area of grid cells This currently doesn't work
 	area = xr.open_dataset('data/area_files/processed_nsidc.nc').area
@@ -118,7 +137,6 @@ def plot_6_timeseries(SIC, LIC, temperature):
 	LIC = LIC.sel(time=slice('2002-01-01','2019-12-31'))
 	temperature = temperature.sel(time=slice('2002-01-01','2019-12-31'))
 
-	landmask = LIC.sum(dim='time') !=0
 	# fig = plt.figure()
 	# ax = plt.axes(projection =ccrs.SouthPolarStereo())
 	# ax.contourf(landmask.x,landmask.y,landmask)
@@ -174,6 +192,12 @@ def plot_6_timeseries(SIC, LIC, temperature):
 		 # Land variables
 		if i==0:
 			data = temperature.where(landmask)
+			for variable in data:
+				subdata = data[variable]
+				gradient = subdata.sum(dim=('x','y')).polyfit(dim='time', deg=1)
+				gradient_m = gradient.sel(degree=1)
+				gradient_b = gradient.sel(degree=0)
+				predicted[variable] = gradient_m['polyfit_coefficients'].values * temperature.time.values.astype(float) + gradient_b['polyfit_coefficients'].values
 			if j == 0:
 				ax.plot(LIC.time, LIC.sum(dim=('x','y')))
 			else:
@@ -182,13 +206,19 @@ def plot_6_timeseries(SIC, LIC, temperature):
 				p_sst = ax.plot([],[])
 				p_skt = ax.plot(data.time, data.skt.sum(dim=('x','y')), label = 'skt')
 				
-				ax.plot(predicted.time, predicted.t2m, color = p_t2m[0].get_color())
-				ax.plot(predicted.time, predicted.sst, color = p_sst[0].get_color())
-				ax.plot(predicted.time, predicted.skt, color = p_skt[0].get_color())
+				ax.plot(data.time, predicted.t2m, color = p_t2m[0].get_color())
+				# ax.plot(data.time, predicted.sst, color = p_sst[0].get_color())
+				ax.plot(data.time, predicted.skt, color = p_skt[0].get_color())
 
 		 # Sea variables
 		elif i==1:
 			data = temperature.where(~landmask).where(SIC!=0)
+			for variable in data:
+				subdata = data[variable]
+				gradient = subdata.sum(dim=('x','y')).polyfit(dim='time', deg=1)
+				gradient_m = gradient.sel(degree=1)
+				gradient_b = gradient.sel(degree=0)
+				predicted[variable] = gradient_m['polyfit_coefficients'].values * temperature.time.values.astype(float) + gradient_b['polyfit_coefficients'].values
 			if j == 0:
 				ax.plot(SIC.time, SIC.sum(dim=('x','y')))
 			else:
@@ -196,6 +226,10 @@ def plot_6_timeseries(SIC, LIC, temperature):
 				p_t2m = ax.plot(data.time, data.t2m.sum(dim=('x','y')), label = 't2m')
 				p_sst = ax.plot(data.time, data.sst.sum(dim=('x','y')), label = 'sst')
 				p_skt = ax.plot(data.time, data.skt.sum(dim=('x','y')), label = 'skt')
+
+				ax.plot(data.time, predicted.t2m, color = p_t2m[0].get_color())
+				ax.plot(data.time, predicted.sst, color = p_sst[0].get_color())
+				ax.plot(data.time, predicted.skt, color = p_skt[0].get_color())
 
 		elif i==2:
 			data = temperature.copy()
@@ -206,6 +240,9 @@ def plot_6_timeseries(SIC, LIC, temperature):
 				p_t2m = ax.plot(data.time, data.t2m.sum(dim=('x','y')), label = 't2m')
 				p_sst = ax.plot(data.time, data.sst.sum(dim=('x','y')), label = 'sst')
 				p_skt = ax.plot(data.time, data.skt.sum(dim=('x','y')), label = 'skt')
+				ax.plot(data.time, predicted.t2m, color = p_t2m[0].get_color())
+				ax.plot(data.time, predicted.sst, color = p_sst[0].get_color())
+				ax.plot(data.time, predicted.skt, color = p_skt[0].get_color())
 	lines = p_sst + p_skt + p_t2m
 	labels = [line.get_label() for line in lines]
 	plt.legend(lines,labels,bbox_to_anchor=(0.99, -0.2), ncol = 4, loc = 'upper right')
@@ -213,18 +250,13 @@ def plot_6_timeseries(SIC, LIC, temperature):
 	misc.savefigures(folder='images/week8',filename='six_timeseries')
 
 
-def plot_2_trends(SIC, LIC, temperature):
-
-
-
+def plot_2_trends(SIC, LIC, temperature, landmask):
 	# Creating figure and setting layout of axes
 	plt.style.use('stylesheets/timeseries.mplstyle')
 	fig, (ax1,ax2) = plt.subplots(2,1,figsize=(5,10), subplot_kw=dict(projection=ccrs.SouthPolarStereo()))
 	fig.tight_layout()
 	SIC = SIC.sel(time=slice('2002-01-01','2019-12-31'))
 	temperature = temperature.sel(time=slice('2002-01-01','2019-12-31'))
-
-	landmask = LIC.sum(dim='time') !=0
 
 	# Getting Trend of different variables
 	
@@ -250,13 +282,12 @@ def plot_2_trends(SIC, LIC, temperature):
 	misc.savefigures(folder='images/week8',filename='seaice_temperature_trends')
 
 
-def plot_6_trends(SIC, LIC, temperature):
+def plot_6_trends(SIC, LIC, temperature, landmask):
 
 	SIC = SIC.sel(time=slice('2002-01-01','2019-12-31'))
 	LIC = LIC.sel(time=slice('2002-01-01','2019-12-31'))
 	temperature = temperature.sel(time=slice('2002-01-01','2019-12-31'))
 
-	landmask = LIC.sum(dim='time') !=0
 	# fig = plt.figure()
 	# ax = plt.axes(projection =ccrs.SouthPolarStereo())
 	# ax.contourf(landmask.x,landmask.y,landmask)
@@ -331,3 +362,56 @@ def plot_6_trends(SIC, LIC, temperature):
 	# lines = p_sst + p_skt + p_t2m
 	# labels = [line.get_label() for line in lines]
 	# plt.legend(lines,labels,bbox_to_anchor=(0.99, -0.2), ncol = 4, loc = 'upper right')
+
+
+def plot_2_scatter(SIC, LIC, temperature, landmask):
+
+	# Normalizing variables by area of grid cells This currently doesn't work
+	area = xr.open_dataset('data/area_files/processed_nsidc.nc').area
+	SIC *= area
+	LIC *= area
+	temperature['sst'] = temperature['sst'] * area / area.sum()
+	temperature['skt'] = temperature['skt'] * area / area.sum()
+	temperature['t2m'] = temperature['t2m'] * area / area.sum()
+	
+	SIC = SIC.sel(time=slice('2002-01-01','2019-12-31'))
+	LIC = LIC.sel(time=slice('2002-01-01','2019-12-31'))
+	temperature = temperature.sel(time=slice('2002-01-01','2019-12-31'))
+
+	# Getting Trend of different variables
+	predicted = xr.Dataset()
+
+	# Creating figure and setting layout of axes
+	plt.style.use('stylesheets/timeseries.mplstyle')
+	fig, axes = plt.subplots(2,1, figsize=(5,10))
+	fig.tight_layout()
+
+
+	rows = ['Land', 'Sea']
+
+	for ax, row in zip(axes, rows):
+	    ax.set_ylabel(row, size = 'large')
+
+	j = 0
+	for i in range(2):
+		ax = axes[i]
+		ax.axhline(0,color='k', alpha=0.5)
+		ax.axvline(0,color='k', alpha=0.5)
+
+		 # Land variables
+		if i==0:
+			data = temperature.where(landmask)
+			if j == 0:
+				ax.scatter(data.t2m.values.flatten(), LIC.values.flatten())
+
+		 # Sea variables
+		elif i==1:
+			data = temperature.where(~landmask).where(SIC!=0)
+			if j == 0:
+				ax.scatter(data.t2m.values.flatten(), SIC.values.flatten())
+
+	# lines = p_sst + p_skt + p_t2m
+	# labels = [line.get_label() for line in lines]
+	# plt.legend(lines,labels,bbox_to_anchor=(0.99, -0.2), ncol = 4, loc = 'upper right')
+
+	# misc.savefigures(folder='images/week8',filename='six_timeseries')
